64098977eede01668be3d74fc2293ea33cf94df7
NguyenQuocHuyGITHUB <122158094+Nine99Huy8181@users.noreply.github.com>
2025-04-05 11:10:37 +0700

    2. API Overview

---
+++ TH_02_04_25/TH_02_04_25/src/components/Statistics.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Statistics({st}) {
+    const [img, setImg] = useState("../images/Button 1509.png");
+    const [color, setColor] = useState("rgb(255 240 245)")
+
+    useEffect(() => {
+        if(st.name === "Profit"){
+            setImg("../images/Button 1529.png")
+            setColor("rgb(239 246 255)")
+        }
+        else if(st.name === "New customer"){
+            setImg("../images/Button 1530.png");
+            setColor("rgb(239 246 255)");
+        }
+    }, [])
+
+  return (
+    <div style={{backgroundColor: color, display: "flex",
+     width: "25%", borderRadius: "10px", justifyContent: "space-between", padding: "10px 30px"}}>
+        <div style={{fontFamily: "sans-serif"}}>
+            <p style={{fontWeight: "bold", color: "black", fontSize: "18px"}}>{st.name}</p>
+            {
+                st.name === "New customer" ? 
+                (<p style={{fontWeight: "bold", fontSize: "40px", margin: "10px 0px"}}>{st.value}</p>)
+                : <p style={{fontWeight: "bold", fontSize: "40px", margin: "10px 0px"}}>${st.value}</p>
+            }
+            {
+                st.percent > 0 ?
+                (
+                    <p  style={{display: "flex"}}>
+                        <img src="../images/icons8-triangle-15.png" alt="" style={{marginRight: "4px", objectFit: "contain"}}/>
+                        <span style={{color: "green", fontWeight: "bold"}}>{st.percent}% </span>
+                        <span style={{color: "gray", marginLeft: "4px"}}>period of change</span>
+                    </p>
+                )
+                :
+                (
+                    <p  style={{display: "flex"}}>
+                        <img src="../images/icons8-triangle-arrow-15.png" alt="" style={{marginRight: "4px", objectFit: "contain"}}/>
+                        <span style={{color: "red", fontWeight: "bold"}}>{st.percent}% </span>
+                        <span style={{color: "gray", marginLeft: "4px"}}>period of change</span>
+                    </p>
+                )
+            }
+        </div>
+        <div>
+            <img src={img} alt="" />
+        </div>
+    </div>
+  )
+}
--- TH_02_04_25/TH_02_04_25/src/layout/MainLayout.jsx
+++ TH_02_04_25/TH_02_04_25/src/layout/MainLayout.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MainLayout.css'
+import Statistics from '../components/Statistics'
 
 export default function MainLayout() {
+    const [data, setData] = useState([])
+    
+    useEffect(() => {
+        fetch("https://67c81bf20acf98d07084e0cf.mockapi.io/Statistics")
+    .then(res => res.json())
+    .then(data => setData(data))
+
+    }, [])
+
   return (
     <div id='main'>
         <div id='menu'>
             <ul>
-                <li><p>Logo</p></li>
-                <li><a href="">Dash board</a></li>
-                <li><a href="">Project</a></li>
-                <li><a href="">Teams</a></li>
-                <li><a href="">Analytics</a></li>
-                <li><a href="">Messages</a></li>
-                <li><a href="">Integrations</a></li>
+                <img src="../images/Image 1858.png" alt="" />
+                <li style={{paddingLeft: "10px"}}><img src="../images/Pie chart.png" alt="" /><a href="">Dash board</a></li>
+                <li style={{paddingLeft: "10px"}}><img src="../images/Folder.png" alt="" /><a href="">Project</a></li>
+                <li style={{paddingLeft: "10px"}}><img src="../images/Groups.png" alt="" /><a href="">Teams</a></li>
+                <li style={{paddingLeft: "10px"}}><img src="../images/Pie chart.png" alt="" /><a href="">Analytics</a></li>
+                <li style={{paddingLeft: "10px"}}><img src="../images/Chat.png" alt="" /><a href="">Messages</a></li>
+                <li style={{paddingLeft: "10px"}}><img src="../images/Code.png" alt="" /><a href="">Integrations</a></li>
             </ul>
             <div id='try-now'>
-                Try now
+                <h2>V2.0 is available</h2>
+                <button>Try now</button>
             </div>
         </div>
         <div id='header'>
-            <p>Dash board</p>
+            <p style={{color: "#ec4d84", fontWeight: "bold", fontSize: "25px", fontFamily: "sans-serif"}}>Dashboard</p>
             <div id='content-header'>
-                <input type="text" name="" id=""/>
+                <input type="text" name="" id="search" placeholder='Search...'/>
                 <img src="../images/Bell 1.png" alt="" />
                 <img src="../images/Question 1.png" alt="" />
                 <img src="../images/Avatar (1).png" alt=""/>
@@ -29,22 +40,34 @@ export default function MainLayout() {
         </div>
         <div id='content'>
             <div>
-                <div>
-                    <img src="" alt="" />
-                    <p>Overview</p>
+                <div style={{display: "flex", alignItems: "center", marginBottom: "20px"}}>
+                    <img src="../images/Squares four 1.png" alt="" style={{marginRight: "5px"}}/>
+                    <span style={{fontFamily: "sans-serif", fontWeight: "bold", fontSize: "20px"}}>Overview</span>
                 </div>
                 <div id='div-component'>
-                    <div>Component</div>
-                    <div>Component</div>
-                    <div>Component</div>
+                    {
+                        data.map((st) => {
+                            return(
+                                <Statistics st={st}></Statistics>
+                            )
+                        })
+                    }
                 </div>
             </div>
-            <div>
-                <div>
-                    <p>Detail report</p>
+            <div style={{marginTop: "30px"}}>
+                <div style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                    <div style={{display: "flex", alignItems: "center"}}>
+                        <img src="../images/File text 1.png" alt="" style={{marginRight: "5px"}}/>
+                        <span style={{fontFamily: "sans-serif", fontWeight: "bold", fontSize: "20px"}}>Detail report</span>
+                    </div>
+                    <div>
+                        <button id='import'>Import</button>
+                        <button id='export'>Export</button>
+                    </div>
                 </div>
                 <div id='div-table'>
                     <table>
+                        <tbody>
                         <tr>
                             <th>Cusotomer name</th>
                             <th>Compan</th>
@@ -73,6 +96,7 @@ export default function MainLayout() {
                             <td>Quoc Huy</td>
                             <td>Quoc Huy</td>
                         </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
